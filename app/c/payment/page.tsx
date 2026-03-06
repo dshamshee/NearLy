f1e1f5b23efdae74f801cf98b786c0cf64843c68
Danish <danishshamshee123@gmail.com>
2026-03-06 15:26:26 +0530

    something changes on dev

--- app/c/payment/page.tsx
+++ app/c/payment/page.tsx
@@ -28,6 +28,15 @@ export default function Payment() {
     return (
         <div className="flex flex-col items-center justify-center h-screen w-full">
             <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
+            <h1>Payment</h1>
         </div>
     )
 }
